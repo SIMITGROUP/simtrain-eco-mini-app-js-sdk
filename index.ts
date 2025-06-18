824590d115f3366a868f460957f2c7853085fe44
jhyong <jhyong@simitgroup.com>
2025-06-18 17:26:04 +0800

    Update

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export { SimtrainEcoMiniAppJsSdk } from "./src/sdk";
+export * from "./src/sdk";
 export * from "./src/types/resource-mapper.type";
 export * from "./src/types/bridge.type";
 export * from "./src/types/service.type";
