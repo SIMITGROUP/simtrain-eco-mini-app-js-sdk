96278a5de33ab6891eb557bff527fb73ce066f04
jhyong <jhyong@simitgroup.com>
2026-06-03 09:15:58 +0800

    chore: remove online payment merchant config bridge service

--- src/services/bridge-resource-accessor.service.ts
+++ src/services/bridge-resource-accessor.service.ts
@@ -1,21 +1,9 @@
 import { MiniAppBridgeResourceAccessorBase } from "./bridge-resource-accessor-base.service";
-import { MiniAppOnlinePaymentMerchantConfigBridgeService } from "./customs/online-payment-merchant-config-bridge.service";
 
 export class MiniAppBridgeResourceAccessor extends MiniAppBridgeResourceAccessorBase {
-  protected customInstances: Partial<{
-    // onlinePaymentMerchantConfig: MiniAppOnlinePaymentMerchantConfigBridgeService;
-  }> = {};
+  protected customInstances: Partial<{}> = {};
 
   constructor() {
     super();
   }
-
-  // ! Danger API, Shouldn't Expose
-  // get onlinePaymentMerchantConfig() {
-  //   if (!this.customInstances.onlinePaymentMerchantConfig) {
-  //     this.customInstances.onlinePaymentMerchantConfig =
-  //       new MiniAppOnlinePaymentMerchantConfigBridgeService(this.bridge);
-  //   }
-  //   return this.customInstances.onlinePaymentMerchantConfig;
-  // }
 }
